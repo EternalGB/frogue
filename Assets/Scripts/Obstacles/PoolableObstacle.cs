ce0f7134f2b26e075091fe05dfa6f86def2ed924
RaphByrne <raph.byrne@gmail.com>
2014-08-26 16:27:25 +0800

    obstacles were being recycled too quickly

--- Assets/Scripts/Obstacles/PoolableObstacle.cs
+++ Assets/Scripts/Obstacles/PoolableObstacle.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PoolableObstacle : PoolableObject
 {
 
-	float recycleOffset = 8;
+	float recycleOffset = 20;
 	public delegate void DestroyHandler(GameObject destroyed);
 	public static event DestroyHandler ObjDestroyed;
 

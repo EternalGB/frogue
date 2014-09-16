0a56d807913b675589f11f62ca1ae1197f4f645c
RaphByrne <raph.byrne@gmail.com>
2014-09-16 12:12:19 +0800

    fixed a bug where ladybugs would always oscillate around y=0 instead of their starting y. Also the medusahead script allows the magnitude of the oscillation to be changed. Increased this to 2 for ladybugs

--- Assets/Scripts/Obstacles/Behaviours/MedusaHead.cs
+++ Assets/Scripts/Obstacles/Behaviours/MedusaHead.cs
@@ -6,12 +6,19 @@ public class MedusaHead : MonoBehaviour
 
 	public float horiVelocity;
 	public float oscillationRate;
+	public float oscillationSize;
 	Vector2 pos;
+	Vector2 startingPos;
+
+	void OnEnable()
+	{
+		startingPos = transform.position;
+	}
 
 	void Update()
 	{
 		pos.x = transform.position.x + horiVelocity*Time.deltaTime;
-		pos.y = Mathf.Sin(Mathf.Repeat(oscillationRate*Time.time,2*Mathf.PI));
+		pos.y = startingPos.y + oscillationSize*Mathf.Sin(Mathf.Repeat(oscillationRate*Time.time,2*Mathf.PI));
 		rigidbody2D.MovePosition(pos);
 	}
 
